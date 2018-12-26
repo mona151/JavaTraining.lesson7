f508d96dfe90677780c60f05153a7f3a9facc6b3
Kirill <nosolution01@gmail.com>
2018-12-26 23:50:40 +0400

    Minor fix of annotation

--- src/annotations/RestrictedNumericValue.java
+++ src/annotations/RestrictedNumericValue.java
@@ -1,11 +1,9 @@
 package annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Target(ElementType.FIELD)
 public @interface RestrictedNumericValue {
 
